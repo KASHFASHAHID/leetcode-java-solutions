1b0ffd185c1c2494471abbcf26fe365e228a45fb
Kashfa Shahid <shahidkashfa@gmail.com>
2026-08-05 23:02:07 +0530

    Time: 4 ms (36.84%), Space: 43.5 MB (15.57%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public boolean isValid(String s) {
+        Map<Character, Character> map = new HashMap<>();
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+            if (map.containsKey(ch)) { // Closing bracket
+                if (stack.isEmpty() || stack.pop() != map.get(ch)) {
+                    return false;
+                }
+            } else { // Opening bracket
+                stack.push(ch);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
